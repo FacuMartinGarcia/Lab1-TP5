2cb3d197fc10f188758363702212175e2e19c495
FacuMartinGarcia <estudiodesarrolladorsoftware@gmail.com>
2024-08-29 10:38:52 -0300

    Inicio
    solo clases

---
+++ src/Entidades/Directorio.java
@@ -0,0 +1,22 @@
+public class Directorio {
+
+    private TreeMap<Long,Cliente>;
+
+    public Directorio() {
+    }
+
+    public void agregarContacto() {
+    }
+
+    public void buscarContacto() {
+    }
+
+    public void buscarTelefono() {
+    }
+
+    public void buscarContactos() {
+    }
+
+    public void borrarContacto() {
+    }
+}
